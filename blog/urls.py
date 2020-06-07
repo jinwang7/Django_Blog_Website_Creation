757c83dc8c9c8274a05c5ec392b3dbd0360ca4c3
jinwang7 <jlwon53@hotmail.com>
2020-06-07 16:43:45 +0000

    First Time Upload Django Web Project

---
+++ blog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import (
+            PostListView,
+            PostDetailView,
+            PostCreateView,
+            PostUpdateView,
+            PostDeleteView,
+            UserPostListView,
+            )
+from . import views
+
+urlpatterns = [
+    path('', PostListView.as_view(), name = 'blog-home'),   # <app>/<model>_<viewtype>.html
+    path('user/<str:username>/', UserPostListView.as_view(), name = 'user-posts'),   # <app>/<model>_<viewtype>.html
+    path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),   # <app>/<model>_<viewtype>.html
+    path('post/new/', PostCreateView.as_view(), name = 'post-create'),   # <app>/<model>_<viewtype>.html
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post-update'),   # <app>/<model>_<viewtype>.html
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = 'post-delete'),   # <app>/<model>_<viewtype>.html
+    path('about/', views.about, name = 'blog-about'),
+]
